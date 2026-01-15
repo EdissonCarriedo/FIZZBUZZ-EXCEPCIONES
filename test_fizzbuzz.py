3ab08bbe1e45c9e0954dc2ff2f932a4959018cbe
EdissonCarriedo <ecarriedochacon@gmail.com>
2026-01-15 12:29:27 +0100

    Add files via upload

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,11 +1,18 @@
 def test_verif_%3()
 # aqui paso un numero y verifico si es multiplo de 3
+# si es igual a 0 es OK sino Error
 pass
 
 def test_verif_%5()
 # aqui paso un numero y verifico si es multiplo de 5
+# si es igual a 0 es OK sino Error
 pass
 
 def test_verif_%3%5()
 # aqui paso un numero y verifico que sea multiplo de ambos
+# si es igual a 0 para ambos es OK sino Error
+pass
+
+def test_verif_valor()
+# aqui paso un valor daistinto al permitido si vale pasa sino error
 pass
