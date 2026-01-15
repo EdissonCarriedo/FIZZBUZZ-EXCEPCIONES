594811581ba8102759e53c8a7a40c845536d2807
EdissonCarriedo <ecarriedochacon@gmail.com>
2026-01-15 12:19:36 +0100

    Add files via upload

---
+++ test_fizzbuzz.py
@@ -0,0 +1,11 @@
+def test_verif_%3()
+# aqui paso un numero y verifico si es multiplo de 3
+pass
+
+def test_verif_%5()
+# aqui paso un numero y verifico si es multiplo de 5
+pass
+
+def test_verif_%3%5()
+# aqui paso un numero y verifico que sea multiplo de ambos
+pass
